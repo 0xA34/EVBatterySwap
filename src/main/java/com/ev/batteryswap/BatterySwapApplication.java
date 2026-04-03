b4d94ae596ada251deb1ea04b0ba538b2932881f
0xA34 <yunaswiss@protonmail.ch>
2026-04-03 21:41:10 +0700

    Add Necessaries.

---
+++ src/main/java/com/ev/batteryswap/BatterySwapApplication.java
@@ -0,0 +1,13 @@
+package com.ev.batteryswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterySwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatterySwapApplication.class, args);
+	}
+
+}
