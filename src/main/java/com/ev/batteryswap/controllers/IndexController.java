4d6ad0d25cc15136294864a95f4539ed4b85d0c1
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2026-04-10 16:04:30 +0700

    Update the basic user interface.

--- src/main/java/com/ev/batteryswap/controllers/IndexController.java
+++ src/main/java/com/ev/batteryswap/controllers/IndexController.java
@@ -1,4 +1,25 @@
 package com.ev.batteryswap.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+
 }
